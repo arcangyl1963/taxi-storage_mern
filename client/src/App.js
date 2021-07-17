9a050519820c6e8abd10bb073e64353752b5fc96
Jim Harris <arcangyl@gmail.com>
2021-07-17 12:11:04 -0400

    Added components directory and navbar component; installed two new dependencies; updated the App script; add the auth.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import CustomerPortal from './pages/CustomerPortal';
+import OperationsPortal from './pages/OperationsPortal';
+import Navbar from './components/navbar';
 
 
 
 const client = new ApolloClient({
   uri: '/graphql',
   cache: new InMemoryCache(),
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
 });
 
 function App() {
   return (
     <ApolloProvider client={client}>   
-        <div className="container">
-          <CustomerPortal />
-        </div>
+        <Router>
+          <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={CustomerPortal} />
+            <Route exact path='/OperationsPortal' component={OperationsPortal} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+          </>
+        </Router>
     </ApolloProvider>
   );
 }
