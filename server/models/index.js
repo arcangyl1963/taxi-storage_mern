8712fc72b9faf266e871f920b1cd367ef01e844b
Mikebird2414 <Mikebird2414@yahoo.com>
2021-07-20 18:53:04 -0400

    Models Update

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,5 @@
 const Customer = require('./Customer');
 const Boxes = require('./Boxes');
+const Operator = require('./Operator');
 
-module.exports = { Customer, Boxes };
+module.exports = { Customer, Boxes, Operator };
