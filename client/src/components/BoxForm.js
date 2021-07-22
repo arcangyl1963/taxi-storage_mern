b8645bd3596877930a2aa6723e29580d8b3ca0d0
Jonathan Rein <jrein1296@gmail.com>
2021-07-22 17:11:49 -0400

    Adding client/utils/queries.js.

--- client/src/components/BoxForm.js
+++ client/src/components/BoxForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
-import { ADD_BOX_TO_USER } from '../../utils/mutations';
+import { REMOVE_BOX_FROM_CUSTOMER } from '../../utils/mutations';
 import Auth from '../../utils/auth';
 
 const BoxForm = ({ customerId }) => {
